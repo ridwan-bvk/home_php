4a9ac0794576415548209be00a8ed670fa0dd026
Ridwan <64180648+ridwan-bvk@users.noreply.github.com>
2021-03-03 06:32:48 +0700

    penembahan file
    penambahan baru

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+include "koneksi.php";
+// include "login.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<br/>
+	<br/>
+	<center></center>	
+	<br/>
+	<div class="login">
+    <br/>
+    <div style="color: red;margin-bottom: 15px;">
+    <?php
+    // Cek apakah terdapat cookie dengan nama message
+    if(isset($_COOKIE["message"])){ // Jika ada
+      echo $_COOKIE["message"]; // Tampilkan pesannya
+    }
+    ?>
+  </div>
+		<form action="login.php" method="post" onSubmit="return validasi()">
+			<div>
+				<label>Username:</label>
+				<input type="text" name="username" id="username" />
+			</div>
+			<div>
+				<label>Password:</label>
+				<input type="password" name="password" id="password" />
+			</div>			
+			<div>
+				<input type="submit" value="Login" class="tombol">
+			</div>
+		</form>
+	</div>
+</body>
+
+<script type="text/javascript">
+	function validasi() {
+		var username = document.getElementById("username").value;
+		var password = document.getElementById("password").value;		
+		if (username != "" && password!="") {
+			return true;
+		}else{
+			alert('Username dan Password harus di isi !');
+			return false;
+		}
+	}
+
+</script>
+</body>
+</html>
+
+<!-- <h2>Modal Login Form</h2>
+
+<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
+
+<div id="id01" class="modal">
+  
+  <form class="modal-content animate" action="login.php" method="post">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+      <img src="" alt="Avatar" class="avatar">
+    </div>
+
+    <div class="container">
+      <label for="username"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="username"  id="username"  required>
+
+      <label for="password"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="password" id="password" required>
+        
+      <button type="submit">Login</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+      <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+  </form>
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script> -->
+
---
+++ login.php
@@ -0,0 +1,26 @@
+<?php 
+include 'koneksi.php';
+// var_dump($_POST);
+// echo($_POST);
+
+
+
+// $username = $_POST['username'];
+// $password = md5($_POST['password']);
+// $koneksi =  $_POST['conn'];
+// $login = mysqli_query($koneksi,"select * from user where username='$username' and password='$password'");
+// $result = mysqli_fetch_array($login);
+// $cek = mysqli_num_rows($login);
+
+// if($cek > 0){
+// 	session_start();
+// 	$_SESSION['username'] = $username;
+// 	$_SESSION['status'] = "login";
+// 	header("location:admin/index.php");
+// }else{
+ 
+// 	header("location:index.php");	
+// }
+
+?>
+<htm>a</htm>
